1765eeebb318af6c5911eec5bbcbafd25990a4e7
Sergei Basharov <sergei@basharov.net>
2025-07-22 16:43:07 +0300

    cleanup

--- app.ts
+++ app.ts
@@ -15,39 +15,17 @@ app.set('port', 4000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-
 app.get('/', routes.index);
 app.get('/partials/channel-content', routes.channel_content);
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser());
 app.use(methodOverride());
 
-
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
-    // runFileWatcher();
 });
 
 io = io.listen(app.get('port') + 1);
 io.sockets.on('connection', socket);
-
-/*
-function runFileWatcher() {
-    fs.watch('./data', function (event, filename) {
-        if (filename && filename.indexOf(".txt") !== -1) {
-            fs.exists('./data/' + filename, function (exists) {
-                if (exists) {
-                    console.log('added: ' + filename);
-                    io.sockets.emit('file:added', {filename: filename});
-                }
-                else {
-                    console.log('removed: ' + filename);
-                    io.sockets.emit('file:removed', {filename: filename});
-                }
-            });
-        }
-    });
-}*/
--- zip.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd /Users/sergeibasharov/works/tv/files
-zip -X -r tv.zip .
-open /Users/sergeibasharov/works/tv/files
